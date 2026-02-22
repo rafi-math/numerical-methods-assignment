1a3a95ab6d215e08193463fc856e02b72d84a103
rafi-math <sbrafi10@gmail.com>
2026-02-23 01:14:06 +0600

    Add Newton backward interpolation

---
+++ backward_interpolation.py
@@ -0,0 +1,29 @@
+def newton_backward(x, f, value):
+    n = len(f)
+    diff = [f[:]]
+    for i in range(1, n):
+        row = []
+        for j in range(n - i):
+            row.append(diff[i-1][j+1] - diff[i-1][j])
+        diff.append(row)
+
+    h = x[1] - x[0]
+    u = (value - x[-1]) / h
+
+    result = f[-1]
+    fact = 1
+    u_term = 1
+
+    for i in range(1, n):
+        u_term = u_term * (u + (i - 1))
+        fact = fact * i
+        result = result + (u_term / fact) * diff[i][-1]
+
+    return result
+
+
+x = list(map(float, input("Enter x values: ").split()))
+f = list(map(float, input("Enter f(x) values: ").split()))
+value = float(input("Enter value to find: "))
+
+print(round(newton_backward(x, f, value), 6))
